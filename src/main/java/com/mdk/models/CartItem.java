7f3a0db20e6bd275af725dc526e37d8161f6b3cc
LeHaiDangSPKT <20110243@student.hcmute.edu.vn>
2022-10-16 00:30:43 +0700

    Create Model last 6 table

---
+++ src/main/java/com/mdk/models/CartItem.java
@@ -0,0 +1,31 @@
+package com.mdk.models;
+
+public class CartItem extends AbstractModel<CartItem>{
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Long cartId) {
+        this.cartId = cartId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    private Long cartId    ;
+    private Long productId ;
+    private Long count     ;
+}
